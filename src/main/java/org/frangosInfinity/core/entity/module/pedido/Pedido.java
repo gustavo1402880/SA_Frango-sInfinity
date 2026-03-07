e2acfd04aadb81459f83b5da427d9da04c3a4d2b
EDUARDO GEFFERT DA SILVA <eduardo_silva173@estudante.sesisenai.org.br>
2026-03-06 21:39:34 -0300

    Feat: Implementa Module Entity Pedido/Produto

--- src/main/java/org/frangosInfinity/core/entity/module/pedido/Pedido.java
+++ src/main/java/org/frangosInfinity/core/entity/module/pedido/Pedido.java
@@ -1,4 +1,82 @@
 package org.frangosInfinity.core.entity.module.pedido;
+import org.frangosInfinity.core.entity.module.mesa.Mesa;
+import org.frangosInfinity.core.entity.module.usuario.Atendente;
+import org.frangosInfinity.core.enums.StatusPedido;
+
+import java.time.LocalDateTime;
 
 public class Pedido {
+
+    // Atributos
+
+    private Long id;
+    private String numeroPedido;
+    private LocalDateTime dataHora;
+    private StatusPedido status;
+    private Mesa mesa;
+    private Atendente atendente;
+    private String tipo;
+
+    // Construtores
+
+    public Pedido(){}
+
+    public Pedido(String numeroPedido, LocalDateTime dataHora, StatusPedido status, Mesa mesa, Atendente atendente, String tipo) {
+        this.numeroPedido = numeroPedido;
+        this.dataHora = dataHora;
+        this.status = status;
+        this.mesa = mesa;
+        this.atendente = atendente;
+        this.tipo = tipo;
+    }
+
+    // Getters & Setters
+
+    public String getNumeroPedido() {
+        return numeroPedido;
+    }
+
+    public void setNumeroPedido(String numeroPedido) {
+        this.numeroPedido = numeroPedido;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public StatusPedido getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusPedido status) {
+        this.status = status;
+    }
+
+    public Mesa getMesa() {
+        return mesa;
+    }
+
+    public void setMesa(Mesa mesa) {
+        this.mesa = mesa;
+    }
+
+    public Atendente getAtendente() {
+        return atendente;
+    }
+
+    public void setAtendente(Atendente atendente) {
+        this.atendente = atendente;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 }
--- src/main/java/org/frangosInfinity/core/entity/module/produto/Produto.java
+++ src/main/java/org/frangosInfinity/core/entity/module/produto/Produto.java
@@ -1,4 +1,115 @@
 package org.frangosInfinity.core.entity.module.produto;
 
 public class Produto {
+
+    // Atributos
+
+    private Long id;
+    private String codigo;
+    private String nome;
+    private String descricao;
+    private double preco;
+    private double custo;
+    private int tempoPreparoMinuto;
+    private boolean disponivel;
+    private String imagemUrl;
+    private int vendasUltimos300dias;
+    private double precoPendenteAprovacao;
+
+    // Construtores
+
+    public Produto() {};
+
+    public Produto(String codigo, String nome, String descricao, double preco, double custo, int tempoPreparoMinuto, boolean disponivel, String imagemUrl, int vendasUltimos300dias, double precoPendenteAprovacao) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.custo = custo;
+        this.tempoPreparoMinuto = tempoPreparoMinuto;
+        this.disponivel = disponivel;
+        this.imagemUrl = imagemUrl;
+        this.vendasUltimos300dias = vendasUltimos300dias;
+        this.precoPendenteAprovacao = precoPendenteAprovacao;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public double getCusto() {
+        return custo;
+    }
+
+    public void setCusto(double custo) {
+        this.custo = custo;
+    }
+
+    public int getTempoPreparoMinuto() {
+        return tempoPreparoMinuto;
+    }
+
+    public void setTempoPreparoMinuto(int tempoPreparoMinuto) {
+        this.tempoPreparoMinuto = tempoPreparoMinuto;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    public String getImagemUrl() {
+        return imagemUrl;
+    }
+
+    public void setImagemUrl(String imagemUrl) {
+        this.imagemUrl = imagemUrl;
+    }
+
+    public int getVendasUltimos300dias() {
+        return vendasUltimos300dias;
+    }
+
+    public void setVendasUltimos300dias(int vendasUltimos300dias) {
+        this.vendasUltimos300dias = vendasUltimos300dias;
+    }
+
+    public double getPrecoPendenteAprovacao() {
+        return precoPendenteAprovacao;
+    }
+
+    public void setPrecoPendenteAprovacao(double precoPendenteAprovacao) {
+        this.precoPendenteAprovacao = precoPendenteAprovacao;
+    }
 }

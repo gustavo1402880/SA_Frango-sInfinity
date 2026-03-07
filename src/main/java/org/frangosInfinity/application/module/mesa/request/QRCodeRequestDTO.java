0b5308b17fa51824ed1d631f4d8a88b0b5d22c32
GustavoHPelissari <gustavo.h.pelissari@gmail.com>
2026-03-07 15:47:22 -0300

    Feat: Implementa requisicao QRCodeRequestDTO no módule request de mesa

--- src/main/java/org/frangosInfinity/application/module/mesa/request/QRCodeRequestDTO.java
+++ src/main/java/org/frangosInfinity/application/module/mesa/request/QRCodeRequestDTO.java
@@ -1,4 +1,51 @@
 package org.frangosInfinity.application.module.mesa.request;
 
-public class QRCodeRequestDTO {
+public class QRCodeRequestDTO
+{
+    private String idMesa;
+    private String tokenAcesso;
+    private Integer tempoExpiracaoSgundos;
+
+    public QRCodeRequestDTO() {}
+
+    public QRCodeRequestDTO(String idMesa, String tokenAcesso)
+    {
+        this.idMesa = idMesa;
+        this.tokenAcesso = tokenAcesso;
+    }
+
+    public String getIdMesa()
+    {
+        return idMesa;
+    }
+
+    public void setIdMesa(String idMesa)
+    {
+        this.idMesa = idMesa;
+    }
+
+    public String getTokenAcesso()
+    {
+        return tokenAcesso;
+    }
+
+    public void setTokenAcesso(String tokenAcesso)
+    {
+        this.tokenAcesso = tokenAcesso;
+    }
+
+    public Integer getTempoExpiracaoSgundos()
+    {
+        return tempoExpiracaoSgundos;
+    }
+
+    public void setTempoExpiracaoSgundos(Integer tempoExpiracaoSgundos)
+    {
+        this.tempoExpiracaoSgundos = tempoExpiracaoSgundos;
+    }
+
+    public Boolean isValid()
+    {
+        return idMesa != null && !idMesa.isEmpty() && tokenAcesso != null && !tokenAcesso.isEmpty();
+    }
 }
